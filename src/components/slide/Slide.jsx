c282a0f9cb7d36c889961b7df41656c2a74d945b
AKASH A <akashpersonal.a@gmail.com>
2025-08-21 14:00:04 +0530

    Fix Vercel npm install peer deps

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import "./Slide.scss";
-import Slider from "infinite-react-carousel";
 
-const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+const Slide = ({ children, slidesToShow }) => {
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-          {children}
-        </Slider>
+        <Swiper slidesPerView={slidesToShow} spaceBetween={10}>
+          {React.Children.map(children, (child) => (
+            <SwiperSlide>{child}</SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
